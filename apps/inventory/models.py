d71ae22ac448e8b24efdf3502b50803c84203eae
Salog0d <david.salomon.nava11@gmail.com>
2025-05-18 00:55:15 -0600

    Added project Inventory model

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Inventory(models.Model):
+
+    #warehouse = models.ForeignKey('warehouses.Warehouse', on_delete=models.CASCADE, related_name='inventory')
+    #medicine = models.ForeignKey('medicines.Medicine', on_delete=models.CASCADE, related_name='inventory')
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    min_threshold = models.IntegerField(validators=[MinValueValidator(0)])
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Inventory - Medicine: {self.medicine.name}, Quantity: {self.quantity}"
+
+    def check_stock(self):
+        """Returns True if the quantity is below the minimum threshold."""
+        return self.quantity < self.min_threshold
