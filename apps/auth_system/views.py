1ee276a4cf216ed008241e3180eeb1937361b1dc
Salog0d <david.salomon.nava11@gmail.com>
2025-05-18 19:56:01 -0600

    Corrected bugs from middleware and crossplatform sharing info

--- apps/auth_system/views.py
+++ apps/auth_system/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth import authenticate
 
 from .models import User, Role
